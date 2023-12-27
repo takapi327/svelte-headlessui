5cc4bae939c377ff2ec40c5974a907c5188ec8c6
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-12-27 19:34:34 +0900

    Fix format settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,5 +27,31 @@ module.exports = {
 				parser: '@typescript-eslint/parser'
 			}
 		}
-	]
-};
+	],
+	rules: {
+		'quotes': [2, 'single'],
+		'no-console': 2,
+		'no-var': 2,
+		'max-len': [2, { code: 180 }],
+		'no-inner-declarations': 2,
+		'no-throw-literal': 2,
+		'prefer-const': 2,
+		'prefer-object-spread': 2,
+		'quote-props': [2, 'consistent'],
+		'brace-style': [2, '1tbs', { 'allowSingleLine': true }],
+		'comma-spacing': [2, { 'before': false, 'after': true }],
+		'key-spacing': [2, { 'beforeColon': false, 'afterColon': true, 'align': 'value'  }],
+		'keyword-spacing': [2, { 'before': true }],
+		'space-infix-ops': 2,
+		'semi': [2, 'never', {'beforeStatementContinuationChars': 'never'}],
+		'semi-spacing': [2, {'after': true, 'before': false}],
+		'semi-style': [2, 'first'],
+		'no-extra-semi': 2,
+		'no-unexpected-multiline': 2,
+		'no-unreachable': 2,
+		'svelte/no-dupe-else-if-blocks': 2,
+		'svelte/button-has-type': 2,
+		'svelte/no-at-debug-tags': 2,
+		'svelte/spaced-html-comment': 2,
+	}
+}
