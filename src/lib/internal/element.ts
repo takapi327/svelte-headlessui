151b79aa924eff995bd5edc063ca9aa06ae5a63c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2023-12-27 19:30:20 +0900

    npx prettier -w src/**/*.ts

--- src/lib/internal/element.ts
+++ src/lib/internal/element.ts
@@ -1,23 +1,23 @@
 import { type SvelteComponent } from 'svelte'
 
 const ELEMENTS = [
-	'a',
-	'div',
-	'ul',
-	'ol',
-	'li',
-	'button',
-	'h1',
-	'h2',
-	'h3',
-	'h4',
-	'h5',
-	'h6'
+  'a',
+  'div',
+  'ul',
+  'ol',
+  'li',
+  'button',
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6'
 ] as const
 
 export type Elements = (typeof ELEMENTS)[number]
 export type SupportedElements = Elements | SvelteComponent
 
 export const isValidElement = (element: SupportedElements): boolean => {
-	return !(typeof element === 'string' && !ELEMENTS.includes(element))
+  return !(typeof element === 'string' && !ELEMENTS.includes(element))
 }
--- src/lib/internal/keyboard.ts
+++ src/lib/internal/keyboard.ts
@@ -1,20 +1,20 @@
 // Ref: https://www.w3.org/TR/uievents-key/#named-key-attribute-values
 export enum Keyboard {
-	Space = ' ',
-	Enter = 'Enter',
-	Escape = 'Escape',
-	Backspace = 'Backspace',
+  Space = ' ',
+  Enter = 'Enter',
+  Escape = 'Escape',
+  Backspace = 'Backspace',
 
-	ArrowLeft = 'ArrowLeft',
-	ArrowUp = 'ArrowUp',
-	ArrowRight = 'ArrowRight',
-	ArrowDown = 'ArrowDown',
+  ArrowLeft = 'ArrowLeft',
+  ArrowUp = 'ArrowUp',
+  ArrowRight = 'ArrowRight',
+  ArrowDown = 'ArrowDown',
 
-	Home = 'Home',
-	End = 'End',
+  Home = 'Home',
+  End = 'End',
 
-	PageUp = 'PageUp',
-	PageDown = 'PageDown',
+  PageUp = 'PageUp',
+  PageDown = 'PageDown',
 
-	Tab = 'Tab'
+  Tab = 'Tab'
 }
